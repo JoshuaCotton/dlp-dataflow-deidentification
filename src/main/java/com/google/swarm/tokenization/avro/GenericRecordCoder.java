c1eb8fe9e8f5517f529fa952c640312a7f7f1864
Josh Cotton <josh.cotton@synd.io>
2024-01-08 16:08:03 -0500

    Upgrade to Beam 2.53.0 with required changes

--- src/main/java/com/google/swarm/tokenization/avro/GenericRecordCoder.java
+++ src/main/java/com/google/swarm/tokenization/avro/GenericRecordCoder.java
@@ -24,7 +24,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.coders.AtomicCoder;
-import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.beam.sdk.extensions.avro.coders.AvroCoder;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 
 public class GenericRecordCoder extends AtomicCoder<GenericRecord> {
